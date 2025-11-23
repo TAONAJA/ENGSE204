c910507ecc984984a84c3e113b3ab1d44045c97a
TAONAJA <yannavut2545@gmail.com>
2025-11-23 21:13:28 +0700

    Update lab1_3.java

--- Lab 1/lab1_3.java
+++ Lab 1/lab1_3.java
@@ -7,13 +7,10 @@ public class lab1_3 {
 
         Scanner input = new Scanner(System.in);
 
-        // อ่านค่าบรรทัดเดียวแล้วแปลงเป็นจำนวนเต็ม
         int number = Integer.parseInt(input.nextLine().trim());
 
-        // เช็คว่าเลขคู่หรือคี่
         String result = (number % 2 == 0) ? "Even" : "Odd";
 
-        // แสดงผลลัพธ์
         System.out.println(result);
 
         input.close();

344bb3684e964eed6a82c7704e03b5a274ec5c8d
TAONAJA <yannavut2545@gmail.com>
2025-12-03 16:50:49 +0700

    Update lab1_1.java

--- Lab 1/lab1_1.java
+++ Lab 1/lab1_1.java
@@ -6,15 +6,14 @@ public class lab1_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // อ่านทีละบรรทัด แล้วค่อยแปลงเป็น int
         int firstNumber  = Integer.parseInt(input.nextLine().trim());
         int secondNumber = Integer.parseInt(input.nextLine().trim());
 
         int sum = firstNumber + secondNumber;
 
-        // แสดงผลเฉพาะคำตอบ ตามที่ระบบตรวจต้องการ
         System.out.println(sum);
 
         input.close();
     }
 }
+

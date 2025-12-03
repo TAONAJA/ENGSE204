32704535831d8f93ed5b1582eaf8ec5c4257663b
TAONAJA <yannavut2545@gmail.com>
2025-12-03 16:54:30 +0700

    Update lab1_3.java

--- Lab 1/lab1_3.java
+++ Lab 1/lab1_3.java
@@ -1,18 +1,11 @@
 package lab1_3;
-
 import java.util.Scanner;
-
 public class lab1_3 {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-
         int number = Integer.parseInt(input.nextLine().trim());
-
         String result = (number % 2 == 0) ? "Even" : "Odd";
-
         System.out.println(result);
-
         input.close();
     }
 }
